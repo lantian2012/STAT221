39443b862f1d07fbef059d9131c88af96db020b0
Tian Lan <tianlan2007@gmail.com>
2014-10-04 13:07:40 -0400

    add test performance function

--- HW2/test.R
+++ HW2/test.R
@@ -1,3 +1,25 @@
 source("poissonLogN_MCMC.R")
 source("Lan_Tian_ps2_functions.R")
-hist(logthetaList[[4]][,1])
+library(compiler)
+w = 1:1000
+simYgivenTheta = cmpfun(simYgivenTheta)
+logtheta0 = rnorm(1000, 1, 1)
+theta0 = exp(logtheta0)
+Y = simYgivenTheta(theta0, w, 2)
+library(microbenchmark)
+compare <- microbenchmark(simYgivenTheta(theta0, w, 2), times = 1000)
+compare
+
+test = poisson.logn.mcmc
+thetaInit = cmpfun(thetaInit)
+thetaMHStep = cmpfun(thetaMHStep)
+test = cmpfun(poisson.logn.mcmc)
+thetaPost = cmpfun(thetaPost)
+dp.dtheta = cmpfun(dp.dtheta)
+d2p.dtheta2 = cmpfun(d2p.dtheta2)
+d3p.dtheta3 = cmpfun(d3p.dtheta3)
+simYgivenTheta = cmpfun(simYgivenTheta)
+poisson.logn.mcmc = cmpfun(poisson.logn.mcmc)
+
+compare <- microbenchmark(poisson.logn.mcmc(Y,w), times = 5)
+compare
